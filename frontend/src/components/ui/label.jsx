0baa45a5bc4ab8cc04e786036e75d0fb2d858bc4
pythonicoder <159662601+pythonicoder@users.noreply.github.com>
2026-02-24 19:35:46 +0100

    Delete frontend/src directory

--- frontend/src/components/ui/label.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Label = ({ children, className = "", ...props }) => {
-  return (
-    <label className={`block mb-1 font-medium ${className}`} {...props}>
-      {children}
-    </label>
-  );
-};
